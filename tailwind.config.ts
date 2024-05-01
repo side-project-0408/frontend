c93bd7a2dd2a30290d15cda2257f37e7ebd3d2cd
chosule <89799325+chosule@users.noreply.github.com>
2024-05-01 19:43:50 +0900

    ✨ Add: tailwind.config color 설정 시작해보기

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      backgroundImage: {},
+      colors: {
+        neutral: {
+          0: "ffffff",
+          800: "#141414",
+        },
       },
     },
   },
