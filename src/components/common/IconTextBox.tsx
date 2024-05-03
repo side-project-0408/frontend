4f2f1c3b3ac6449bc8ef4a62c3c43a2c34f40031
chosule <89799325+chosule@users.noreply.github.com>
2024-05-03 17:39:03 +0900

    ✨ Add: iconTextBox 생성

---
+++ src/components/common/IconTextBox.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+type Props = {
+  count: number;
+  icon: string;
+};
+
+export default function IconTextBox({ count, icon }: Props) {
+  return (
+    <div className="flex items-center gap-1">
+      <Image src={icon} alt={icon} />
+      <h3 className="text-xs">{count}</h3>
+    </div>
+  );
+}
