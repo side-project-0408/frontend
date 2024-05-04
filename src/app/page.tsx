52f4f72b0d9e7480c7337e20f55787ec068039ea
Woojin-Choi <panda1996@snu.ac.kr>
2024-05-04 18:43:09 +0900

    Feat: add dummyHotProject

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,90 @@
 import ProjectBox from "@/components/common/ProjectBox";
 import Link from "next/link";
 
+import { faker } from "@faker-js/faker";
+
 export default function Home() {
+  const dummyHotProject = [
+    {
+      projectId: faker.number.int(),
+      nickname: faker.person.fullName(),
+      userFileUrl: faker.image.avatar(),
+      title: faker.lorem.sentences(),
+      techStack: "react, java, next, spring, express",
+      position: "frontend, backend, designer, PM",
+      deadLine: `2024-${faker.number.int({ min: 1, max: 12 })}-${faker.number.int({ min: 1, max: 30 })}`,
+      viewCount: faker.number.int(),
+      favoriteCount: faker.number.int(),
+      createdAt: faker.date.anytime(),
+      recent: faker.datatype.boolean(0.5),
+    },
+    {
+      projectId: faker.number.int(),
+      nickname: faker.person.fullName(),
+      userFileUrl: faker.image.avatar(),
+      title: faker.lorem.sentences(),
+      techStack: "react, java, next, spring, express",
+      position: "frontend, backend, designer, PM",
+      deadLine: `2024-${faker.number.int({ min: 1, max: 12 })}-${faker.number.int({ min: 1, max: 30 })}`,
+      viewCount: faker.number.int(),
+      favoriteCount: faker.number.int(),
+      createdAt: faker.date.anytime(),
+      recent: faker.datatype.boolean(0.5),
+    },
+    {
+      projectId: faker.number.int(),
+      nickname: faker.person.fullName(),
+      userFileUrl: faker.image.avatar(),
+      title: faker.lorem.sentences(),
+      techStack: "react, java, next, spring, express",
+      position: "frontend, backend, designer, PM",
+      deadLine: `2024-${faker.number.int({ min: 1, max: 12 })}-${faker.number.int({ min: 1, max: 30 })}`,
+      viewCount: faker.number.int(),
+      favoriteCount: faker.number.int(),
+      createdAt: faker.date.anytime(),
+      recent: faker.datatype.boolean(0.5),
+    },
+    {
+      projectId: faker.number.int(),
+      nickname: faker.person.fullName(),
+      userFileUrl: faker.image.avatar(),
+      title: faker.lorem.sentences(),
+      techStack: "react, java, next, spring, express",
+      position: "frontend, backend, designer, PM",
+      deadLine: `2024-${faker.number.int({ min: 1, max: 12 })}-${faker.number.int({ min: 1, max: 30 })}`,
+      viewCount: faker.number.int(),
+      favoriteCount: faker.number.int(),
+      createdAt: faker.date.anytime(),
+      recent: faker.datatype.boolean(0.5),
+    },
+    {
+      projectId: faker.number.int(),
+      nickname: faker.person.fullName(),
+      userFileUrl: faker.image.avatar(),
+      title: faker.lorem.sentences(),
+      techStack: "react, java, next, spring, express",
+      position: "frontend, backend, designer, PM",
+      deadLine: `2024-${faker.number.int({ min: 1, max: 12 })}-${faker.number.int({ min: 1, max: 30 })}`,
+      viewCount: faker.number.int(),
+      favoriteCount: faker.number.int(),
+      createdAt: faker.date.anytime(),
+      recent: faker.datatype.boolean(0.5),
+    },
+    {
+      projectId: faker.number.int(),
+      nickname: faker.person.fullName(),
+      userFileUrl: faker.image.avatar(),
+      title: faker.lorem.sentences(),
+      techStack: "react, java, next, spring, express",
+      position: "frontend, backend, designer, PM",
+      deadLine: `2024-${faker.number.int({ min: 1, max: 12 })}-${faker.number.int({ min: 1, max: 30 })}`,
+      viewCount: faker.number.int(),
+      favoriteCount: faker.number.int(),
+      createdAt: faker.date.anytime(),
+      recent: faker.datatype.boolean(0.5),
+    },
+  ];
+
   return (
     <main className="h-auto border border-black">
       <div className="mt-40 flex h-60 w-full items-center justify-center border border-black bg-slate-500">
