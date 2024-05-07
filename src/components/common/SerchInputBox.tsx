94144cdb06dbbebfd15c365c6526ca3da2722cd4
chosule <89799325+chosule@users.noreply.github.com>
2024-05-07 17:26:41 +0900

    ✨ Add: 검색창 공통 컴포넌트 생성

---
+++ src/components/common/SerchInputBox.tsx
@@ -0,0 +1,11 @@
+const SerchInputBox = () => {
+  return (
+    <input
+      type="search"
+      placeholder="검색창"
+      className="border border-neutral-50 rounded-2xl h-[40px] py-2 px-3"
+    />
+  );
+};
+
+export default SerchInputBox;
