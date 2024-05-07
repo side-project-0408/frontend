e55f1a4c338df8f808d614d5caef0f2ba53949f4
chosule <89799325+chosule@users.noreply.github.com>
2024-05-07 17:23:59 +0900

    🐛 Fix: selec box option css 변경으로 인하여 ul , li 태그로 변경해서 적용

--- src/components/common/SelectBox.tsx
+++ src/components/common/SelectBox.tsx
@@ -1,40 +1,54 @@
 "use client";
 import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
 
 export type Props = {
   height?: number;
   width?: number;
+  title: string;
   options: { key: number; value: string }[];
 };
-const SelectBox = ({ height = 40, width = 129, options }: Props) => {
-  const [selected, setSelected] = useState<string>("");
-  const handleOptionClick = (selectOption: string) => {
-    console.log("선택한옵션", selectOption);
+const SelectBox = ({ height = 40, width = 129, options, title }: Props) => {
+  const [optSelected, setOptSelected] = useState<string>("");
+
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const ToggleHandler = () => {
+    setIsOpen((prev) => !prev);
   };
   return (
-    <select
-      name=""
-      id=""
-      value={selected}
-      className={`w-[${width}px]`}
-      onChange={(e) => {
-        setSelected(e.target.value);
-      }}
+    <div
+      className={`w-[${width}px] h-[${height}px] relative cursor-pointer rounded-2xl border`}
+      onClick={ToggleHandler}
     >
-      <>
-        {options?.map((option) => {
-          return (
-            <option
-              className="bg-neutral-500"
-              key={option.key}
-              onClick={() => handleOptionClick(option.value)}
-            >
-              {option.value}
-            </option>
-          );
-        })}
-      </>
-    </select>
+      <div className="absolute top-[50%] flex w-full -translate-y-1/2 transform items-center justify-around">
+        {optSelected ? <span>{optSelected}</span> : <span>{title}</span>}
+        <IoIosArrowDown />
+      </div>
+      <div>
+        {isOpen && (
+          <ul
+            className={`absolute top-[42px] z-[1] w-[${width}px] rounded-xl border bg-neutral-0`}
+          >
+            {options?.map((option) => {
+              return (
+                <>
+                  <li
+                    key={option.key}
+                    className="px-3 py-2 font-bold hover:text-neutral-500"
+                    onClick={() => {
+                      setOptSelected(option.value);
+                    }}
+                  >
+                    {option.value}
+                  </li>
+                </>
+              );
+            })}
+          </ul>
+        )}
+      </div>
+    </div>
   );
 };
 
