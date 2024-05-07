aae3344d5a27526ff0ef8ad02d13f9018dbe1d5f
chosule <89799325+chosule@users.noreply.github.com>
2024-05-08 00:05:23 +0900

    🐛 Fix: 공통 컬러 변경 및 로직 수정

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,10 +1,11 @@
 import BackButton from "@/components/common/BackButton";
 import ContentBox from "@/components/common/ContentBox";
 import SelectBox from "@/components/common/SelectBox";
+import SelectStack from "@/components/common/SelectStack";
 import SerchInputBox from "@/components/common/SerchInputBox";
-import Image from "next/image";
+import TechStack from "@/components/common/TechStack";
 
-const selectOption = [
+const option = [
   { key: 1, value: "프론트엔드" },
   { key: 2, value: "백엔드" },
   { key: 3, value: "디자이너" },
@@ -17,12 +18,15 @@ export default function PeoplePage() {
       {/* 아래 타이틀 공통 컴포넌트로 빼야할듯 */}
       <div className="relative flex items-center gap-4">
         <h1 className="text-[36px] font-bold">People</h1>
-        <p className="title-content relative text-[20px] text-neutral-100">
+        <p className="title-content text-neutral-gray-100 relative text-[20px]">
           우리가 people에게 직접 제안하고 프로젝트를 구성할 수 있어요.
         </p>
       </div>
       <div className="flex justify-between">
-        <SelectBox options={selectOption} />
+        <div className="flex gap-3">
+          <SelectBox options={option} title="포지션" />
+          <SelectStack />
+        </div>
         <SerchInputBox />
       </div>
       <ContentBox />
