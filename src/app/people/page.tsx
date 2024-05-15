acc6f4db932565a8de8dbf6f34ba7cf07434e223
chosule <89799325+chosule@users.noreply.github.com>
2024-05-15 14:35:10 +0900

    🐛 Fix: people 페이지 style 수정 및 router 경로 설정 , searchParams 추가, clickhandler 추가

--- src/app/people/page.tsx
+++ src/app/people/page.tsx
@@ -1,35 +1,87 @@
-import BackButton from "@/components/common/BackButton";
-import ContentBox from "@/components/common/ContentBox";
+"use client";
 import SelectBox from "@/components/common/SelectBox";
 import SelectStack from "@/components/common/SelectStack";
-import SerchInputBox from "@/components/common/SerchInputBox";
-import TechStack from "@/components/common/TechStack";
+import Peoples from "@/components/people/Peoples";
+import SearchForm from "@/components/common/SearchForm";
+import { useRouter } from "next/navigation";
+import { FormEventHandler, useRef, useState } from "react";
 
 const option = [
-  { key: 1, value: "프론트엔드" },
-  { key: 2, value: "백엔드" },
-  { key: 3, value: "디자이너" },
-  { key: 4, value: "기획자" },
+  { key: "frontend", value: "프론트엔드" },
+  { key: "beckend", value: "백엔드" },
+  { key: "designer", value: "디자이너" },
+  { key: "planner", value: "기획자" },
 ];
-export default function PeoplePage() {
+
+type Props = {
+  searchParams: {
+    page: string;
+    size: string;
+    sort?: string;
+    keyword?: string;
+    position?: string;
+    teckStack?: string;
+  };
+};
+export default function PeoplePage({ searchParams }: Props) {
+  const router = useRouter();
+
+  const newSearchParams = new URLSearchParams(searchParams);
+  const [optSelected, setOptSelected] = useState<string>("");
+
+  const [keyword, setKeyword] = useState<string>("");
+
+  const onClickSelectBox = (value: string) => {
+    setOptSelected(value);
+    newSearchParams.set("position", value);
+    router.replace(`/people?${newSearchParams.toString()}`);
+  };
+
+  const ref = useRef<any>();
+
+  const onSubmitSearch: FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    setKeyword(ref.current.value);
+    router.push(`/people?keyword=${ref.current.value}`);
+  };
+
   return (
     <div>
-      <BackButton />
-      {/* 아래 타이틀 공통 컴포넌트로 빼야할듯 */}
-      <div className="relative flex items-center gap-4">
+      <div className="items-cebter relative flex gap-4">
         <h1 className="text-[36px] font-bold">People</h1>
-        <p className="title-content text-neutral-gray-100 relative text-[20px]">
+        <p className="title-content text-neutral-gray-100 relative text-[15px]">
           우리가 people에게 직접 제안하고 프로젝트를 구성할 수 있어요.
         </p>
       </div>
       <div className="flex justify-between">
         <div className="flex gap-3">
-          <SelectBox options={option} title="포지션" />
+          <SelectBox
+            onClick={onClickSelectBox}
+            options={option}
+            title="포지션"
+            optSelected={optSelected}
+          />
           <SelectStack />
         </div>
-        <SerchInputBox />
+        <SearchForm ref={ref} onSubmit={onSubmitSearch} />
       </div>
-      <ContentBox />
+      <div className="flex justify-end gap-3">
+        <button
+          onClick={() => {
+            router.push(`/people?sort=RECENT`);
+          }}
+        >
+          최신순
+        </button>
+        <button
+          onClick={() => {
+            router.push(`/people?sort=POPULAR`);
+          }}
+        >
+          인기순
+        </button>
+      </div>
+      <Peoples searchParams={searchParams} />
     </div>
   );
 }
