d523a03d1801200b110d184487b08a64614a81e5
chosule <89799325+chosule@users.noreply.github.com>
2024-05-16 18:00:35 +0900

    ✨ Add: people상세 페이지 msw 엔드포인트 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -12,6 +12,26 @@ export const handlers = [
   http.get("/peoples", ({ request, params }) => {
     return HttpResponse.json(peoples);
   }),
+  http.get("/peoples/:peopleId", ({ params }) => {
+    const { peopleId } = params;
+    return HttpResponse.json({
+      result: "success",
+      data: {
+        userId: `${peopleId}`,
+        nickname: "닉네임",
+        favoriteCount: 223,
+        viewCount: 336,
+        position: "Backend",
+        userFileUrl: "/Users/user/Desktop/pictures/userPicture.jpg",
+        year: "경력없음",
+        techStack: "react, java, ---",
+        softSkill: "소통, 적극성, ---",
+        links: "http://블로그주소",
+        alarmStatus: true,
+        content: "안녕하세요 구인 중입니다",
+      },
+    });
+  }),
 ];
 
 export default handlers;
