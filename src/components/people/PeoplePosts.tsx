afc6b829bce8f99f5f75997908b94eca2a0ff6bb
chosule <89799325+chosule@users.noreply.github.com>
2024-05-28 15:10:29 +0900

    ✨ Add: 찜하기 버튼 클릭 시 useMutation 적용하여 fetch 및 찜한 사람들 목록에 추가

--- src/components/people/PeoplePosts.tsx
+++ src/components/people/PeoplePosts.tsx
@@ -1,18 +1,25 @@
 "use client";
 import getPeopleDetail from "@/lib/people/getPeopleDetail";
 import { GetPeoplePost, GetPeoples } from "@/model/peoples";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import Image from "next/image";
 import BlueTextBox from "../common/BlueTextBox";
 import HashTag from "../common/HashTag";
 import HeartEyeIconBox from "../common/HeartEyeIconBox";
 import heartIcon from "../../../public/image/heart.svg";
+import fillHeartIcon from "../../../public/image/fillHeart.svg";
 import eyeIcon from "../../../public/image/eye.svg";
 import TechStack from "../common/TechStack";
 import Link from "next/link";
+import { MouseEventHandler, useEffect, useState } from "react";
+import LikePeople from "./LikePeople";
+import getLikePeoples from "@/lib/people/getLikePeoples";
+
 type Props = {
   peopleId: string;
 };
+
+// 추후에 백엔드 데이터 넣었을때 직접 데이터를 가공하지않고 쿼리키를 가져온뒤 버튼 컬러 조정해야함
 
 export default function PeoplePosts({ peopleId }: Props) {
   const { data } = useQuery<
@@ -24,6 +31,14 @@ export default function PeoplePosts({ peopleId }: Props) {
     queryKey: ["get", "peoplesDetail", peopleId],
     queryFn: getPeopleDetail,
   });
+
+  const { data: likePeopleList } = useQuery<GetPeoples>({
+    queryKey: ["get", "likepeoples"],
+    queryFn: getLikePeoples,
+    staleTime: 60 * 1000, // fresh -> stale, 5분이라는 기준
+    gcTime: 300 * 1000,
+  });
+
   const {
     content,
     nickname,
@@ -37,6 +52,80 @@ export default function PeoplePosts({ peopleId }: Props) {
     alarmStatus,
     year,
   } = data?.data ?? {};
+
+  const queryClient = useQueryClient();
+
+  const likeQuery = queryClient.getQueryData<GetPeoples>([
+    "get",
+    "likepeoples",
+  ]);
+  console.log("likeQuery", likeQuery);
+
+  const liked = !!likeQuery?.data.find(
+    (item) => item.userId === Number(peopleId),
+  );
+
+  console.log("liked", liked);
+
+  const like = useMutation({
+    mutationFn: (peopleId: string) => {
+      return fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/users/favorite/${peopleId}`,
+        {
+          method: "post",
+        },
+      );
+    },
+    onMutate(peopleId: string) {
+      const oldData = queryClient.getQueryData<GetPeoples>([
+        "get",
+        "likepeoples",
+      ]);
+      if (oldData) {
+        const newData = {
+          ...oldData,
+          data: oldData.data.map((item) => ({
+            ...item,
+            userId: Number(peopleId),
+          })),
+        };
+        queryClient.setQueryData(["get", "likepeoples"], newData);
+      }
+    },
+  });
+
+  const unLike = useMutation({
+    mutationFn: (peopleId: string) => {
+      return fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/users/favorite/${peopleId}`,
+        {
+          method: "delete",
+        },
+      );
+    },
+    onMutate(peopleId: string) {
+      const oldData = queryClient.getQueryData<GetPeoples>([
+        "get",
+        "likepeoples",
+      ]);
+      if (oldData) {
+        const deleteData = {
+          ...oldData,
+          data: oldData.data.filter((item) => item.userId !== Number(peopleId)),
+        };
+        queryClient.setQueryData(["get", "likepeoples"], deleteData);
+      }
+    },
+  });
+
+  const onLike: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault();
+    if (liked) {
+      unLike.mutate(peopleId);
+    } else {
+      like.mutate(peopleId);
+    }
+  };
 
   return (
     <div className="flex flex-col">
@@ -89,12 +178,24 @@ export default function PeoplePosts({ peopleId }: Props) {
       </div>
       <div className="mt-[60px] flex gap-[13px] self-center">
         <button
-          className={`bg-neutral-orange-500 h-[58px] w-[142px] rounded-md font-bold ${alarmStatus ? "text-neutral-white-0" : "text-neutral-black-800"}`}
+          className={`h-[58px] w-[142px] rounded-md bg-neutral-orange-500 font-bold ${alarmStatus ? "text-neutral-white-0" : "text-neutral-black-800"}`}
         >
           {alarmStatus ? "제안하기" : "제안불가"}
         </button>
-        <button className="flex h-[58px] w-[58px] flex-col items-center justify-center rounded-md border">
-          <Image src={heartIcon} alt="하트 아이콘" />
+        <button
+          onClick={onLike}
+          className="flex h-[58px] w-[58px] flex-col items-center justify-center rounded-md border"
+        >
+          {liked ? (
+            <Image
+              src={fillHeartIcon}
+              alt="하트아이콘"
+              width={20}
+              height={20}
+            />
+          ) : (
+            <Image src={heartIcon} alt="하트아이콘" width={20} height={20} />
+          )}
           <h5 className="text-[12px]">{favoriteCount}</h5>
         </button>
       </div>
