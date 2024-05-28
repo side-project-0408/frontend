09893ce2c537faaa44582478f8deb4a7ac1f8244
chosule <89799325+chosule@users.noreply.github.com>
2024-05-28 15:12:05 +0900

    ✨ Add: msw mocking  찜하기 요청 및 목록 handler 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -32,6 +32,36 @@ export const handlers = [
       },
     });
   }),
+  http.post("/users/favorite/:userId", () => {
+    return HttpResponse.json({
+      favorit: true,
+    });
+  }),
+  http.delete("/users/favorite/:userId", () => {
+    return HttpResponse.text(JSON.stringify("찜하기 삭제"));
+  }),
+  // 내가 찜한 사람 목록 확인
+  http.get("/users/favorite", () => {
+    return HttpResponse.json({
+      result: "success",
+      data: [
+        {
+          userId: 1,
+          nickName: "닉네임bb (중복체크 필요)",
+          userFileUrl: "/Users/user/Desktop/prictures/userPicture.jpg",
+          techStack: "java, spring...",
+          position: "FrontEnd",
+          employmentStatus: false,
+          year: "경력없음",
+          links: "http://블로그주소",
+          alarmStatus: true,
+          content: "안녕하세요 구인 중입니다",
+          softSkill: "소통 능력, 팀원간의 화합, ---",
+          email: "aaa@gmail.com",
+        },
+      ],
+    });
+  }),
 ];
 
 export default handlers;
