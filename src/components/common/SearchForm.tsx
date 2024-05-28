1cc96f632a29539938cb5404c3562278f499b0eb
chosule <89799325+chosule@users.noreply.github.com>
2024-05-28 14:40:42 +0900

    ✨ Add: displayName 추가

--- src/components/common/SearchForm.tsx
+++ src/components/common/SearchForm.tsx
@@ -14,11 +14,13 @@ const SearchForm = forwardRef<HTMLInputElement, Props>(({ onSubmit }, ref) => {
           name="search"
           type="search"
           placeholder="검색창"
-          className="border-neutral-gray-50 h-[40px] rounded-2xl border px-3 py-2"
+          className="h-[40px] rounded-2xl border border-neutral-gray-50 px-3 py-2"
         />
       </form>
     </>
   );
 });
+
+SearchForm.displayName = "SearchForm";
 
 export default SearchForm;
