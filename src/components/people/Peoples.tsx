bb4610c5567ed653e566fcd0b129b16bb840d067
chosule <89799325+chosule@users.noreply.github.com>
2024-06-06 20:48:31 +0900

    ✨ Add: 피플 목록 페이지네이션 작업

--- src/components/people/Peoples.tsx
+++ src/components/people/Peoples.tsx
@@ -3,6 +3,8 @@ import getPeoples from "@/lib/people/getPeoples";
 import { GetPeoples } from "@/model/peoples";
 import { useQuery } from "@tanstack/react-query";
 import PeopleContentBox from "./PeopleContentBox";
+import { useState } from "react";
+import Pagination from "../common/Pagination";
 
 type Props = {
   searchParams: {
@@ -11,34 +13,46 @@ type Props = {
     sort?: string;
     keyword?: string;
     position?: string;
-    teckStack?: string;
+    techStack?: string;
   };
 };
 
 export default function Peoples({ searchParams }: Props) {
+  const [page, setPage] = useState(parseInt(searchParams.page) || 0);
+
   const { data } = useQuery<
     GetPeoples,
     Object,
     GetPeoples,
     [_1: string, _2: string, Props["searchParams"]]
   >({
-    queryKey: ["get", "peoples", searchParams],
+    queryKey: ["get", "peoples", { ...searchParams, page: page.toString() }],
     queryFn: getPeoples,
-    // 데이퍼 패칭 처럼 행동 //content박스에 넘겨주면 리렌더링 됨
   });
+
+  const totalPages = data?.totalPages || 1;
+
   const sortedData = data?.data.sort((a, b) => {
     if (a.recent && !b.recent) return -1;
     if (!a.recent && b.recent) return 1;
     return 0;
   });
+
   if (data?.data.length === 0) {
     return <div>값이 없습니다. 다른걸 검색해주세요!</div>;
   }
+
   return (
-    <div className="grid grid-cols-4 gap-[15px]">
-      {sortedData?.map((contentData) => (
-        <PeopleContentBox key={contentData.userId} contentData={contentData} />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-4 gap-[15px]">
+        {sortedData?.map((contentData) => (
+          <PeopleContentBox
+            key={contentData.userId}
+            contentData={contentData}
+          />
+        ))}
+      </div>
+      <Pagination totalPages={totalPages} page={page} setPage={setPage} />
+    </>
   );
 }
