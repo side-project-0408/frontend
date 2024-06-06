4cabb5370c18547549ac07a5d07050de5c7b663b
chosule <89799325+chosule@users.noreply.github.com>
2024-06-06 18:57:14 +0900

    🐛 Fix: 링크 추가 처음 로그인시 데이터 null값으로 인한 코드 수정

--- src/components/mypage/AddInput.tsx
+++ src/components/mypage/AddInput.tsx
@@ -1,70 +1,63 @@
 "use client";
-import { GetUserData } from "@/model/userInfo";
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { RiDeleteBin5Line } from "react-icons/ri";
-import useDebounce from "./lib/Debounce";
 import { FaPlus } from "react-icons/fa6";
 
 interface Props {
   linkUrls: string;
-  setProduct: React.Dispatch<React.SetStateAction<GetUserData>>;
+  setLink: (newLinks: string[]) => void;
 }
 
-export default function AddInput({ linkUrls, setProduct }: Props) {
+export default function AddInput({ linkUrls, setLink }: Props) {
   const linkRef = useRef<HTMLInputElement>(null);
 
   // 초기 linkUrl값을 배열로 전환
-  const [linkInput, setLinkInput] = useState(() =>
-    typeof linkUrls === "string"
-      ? linkUrls.split(",").map((link) => ({ link }))
-      : [],
-  );
+  const [linkInput, setLinkInput] = useState<string[]>([]);
+
+  useEffect(() => {
+    setLinkInput(linkUrls ? linkUrls.split(",") : [""]);
+  }, [linkUrls]);
 
   const handleClick = () => {
     if (linkInput.length < 3) {
-      setLinkInput([...linkInput, { link: "" }]);
+      setLinkInput([...linkInput, ""]);
     } else {
       alert("링크는 최대 3개 입니다.");
     }
-    setProduct((prev) => ({
-      ...prev,
-      links: linkInput.map((inputLink) => inputLink.link).join(","),
-    }));
+  };
+
+  const handleDelete = (i: number) => {
+    const newInputData = linkInput.filter((_, index) => index !== i);
+    setLinkInput(newInputData.length ? newInputData : [""]);
+    setLink(newInputData);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>, i: number) => {
     const newInputData = linkInput.map((item, index) =>
-      index === i ? { ...item, link: linkRef.current?.value as string } : item,
+      index === i ? e.target.value : item,
     );
     setLinkInput(newInputData);
+    setLink(newInputData);
   };
-
-  const handleDelete = (i: number) => {
-    const deleteVal = [...linkInput];
-    deleteVal.splice(i, 1);
-    setLinkInput(deleteVal);
-    setProduct((prev) => ({
-      ...prev,
-      links: deleteVal.map((inputLink) => inputLink.link).join(","),
-    }));
-  };
-
   return (
     <div className="flex flex-col items-start gap-3">
-      {linkInput?.map((val, i) => (
-        <div className="flex w-full gap-3" key={`input${i}`}>
-          <input
-            name="link"
-            value={val.link}
-            ref={linkRef}
-            onChange={(e) => handleChange(e, i)}
-            className="h-[55px] w-[300px] rounded-md border px-2 py-1"
-          />
-          <button onClick={() => handleDelete(i)} className="text-xl">
-            <RiDeleteBin5Line />
-          </button>
-        </div>
-      ))}
+      {linkInput?.map((val, i) => {
+        return (
+          <div className="flex w-full gap-3" key={`input${i}`}>
+            <input
+              name="link"
+              value={val}
+              ref={linkRef}
+              onChange={(e) => handleChange(e, i)}
+              className="h-[55px] w-[300px] rounded-md border px-2 py-1 placeholder:text-neutral-black-800"
+              placeholder={val}
+            />
+            <button onClick={() => handleDelete(i)} className="text-xl">
+              <RiDeleteBin5Line />
+            </button>
+          </div>
+        );
+      })}
       <button
         onClick={handleClick}
         className="flex items-center gap-1 text-sm font-bold"
