8e168b50a14c629ecf20447219b8c0e9f4199b95
chosule <89799325+chosule@users.noreply.github.com>
2024-06-06 20:36:24 +0900

    ✨ Add: 내가 작성한 프로젝트 페이지 작업

--- src/app/mypage/project-write/page.tsx
+++ src/app/mypage/project-write/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+import NoDataAlert from "@/components/common/NoDataAlert";
+import ProjectBox from "@/components/common/ProjectBox";
+import getWriteProject from "@/lib/mypage/getWriteProject";
+import { GetProjects } from "@/model/projects";
+import { useQuery } from "@tanstack/react-query";
+import { getCookie } from "cookies-next";
+
 export default function ProjectWritePage() {
-  return <div>내가 작성한 프로젝트</div>;
+  const access_token = getCookie("access_token") as string;
+
+  const { data } = useQuery<
+    GetProjects,
+    Error,
+    GetProjects,
+    [string, string, string]
+  >({
+    queryKey: ["get", "writeproject", access_token],
+    queryFn: getWriteProject,
+    enabled: !!access_token,
+  });
+  return (
+    <>
+      {data?.data.length === 0 ? (
+        <NoDataAlert>작성한 프로젝트가 없습니다.</NoDataAlert>
+      ) : (
+        <>{data?.data.map((project) => <ProjectBox project={project} />)}</>
+      )}
+    </>
+  );
 }
