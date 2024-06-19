0083029f0433429dbebb109276ad793af5457287
chosule <89799325+chosule@users.noreply.github.com>
2024-06-19 13:50:39 +0900

    🐛 Fix: test 문구원래대로 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,9 +33,7 @@ export default async function Home({ searchParams }: Props) {
       <div className="flex flex-col gap-[60px]">
         <div className="flex flex-col gap-[20px]">
           <div className="flex justify-between">
-            <div className="text-xl font-bold">
-              이번 주 Hot People test 다시
-            </div>
+            <div className="text-xl font-bold">이번 주 Hot People</div>
             <Link href={`/people`}>전체보기</Link>
           </div>
           <div className="flex flex-wrap items-center gap-[9px]">
