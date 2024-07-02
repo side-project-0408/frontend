12e514f9fa0548d612dd67cbf6a3c55bd6fbe4b8
chosule <89799325+chosule@users.noreply.github.com>
2024-07-02 16:44:23 +0900

    🐛 Fix: 로그인 모달, 모달 컴포넌트 수정

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,25 +1,32 @@
 "use client";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
-import { useRouter } from "next/navigation";
-
-export function Modal({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-
+type Props = {
+  children: React.ReactNode;
+  onClickHandler?: () => void;
+};
+export function Modal({ children, onClickHandler }: Props) {
+  const [close, setClose] = useState<boolean>(false);
   return (
-    <div className="fixed left-0 top-0 z-[4] h-full w-full bg-[rgba(0,0,0,0.4)]">
-      <div className="absolute left-[50%] top-[50%] h-[500px] w-[400px] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-white">
+    <div
+      className={`fixed left-0 top-0 z-[4] h-full w-full bg-[rgba(0,0,0,0.4)] ${close && "hidden"}`}
+    >
+      <div className="absolute left-[50%] top-[50%] h-[500px] w-[400px] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-white p-[11px]">
         <div className="flex justify-end">
           <button
             type="button"
-            className="px-[20px] py-[20px]"
             onClick={() => {
-              router.back();
+              setClose(true);
+              if (onClickHandler) {
+                onClickHandler();
+              }
             }}
           >
-            X
+            <IoClose className="text-[30px]" />
           </button>
         </div>
-        <div className="px-[30px] py-[30px]">{children}</div>
+        <div className="h-full px-[30px] py-[30px]">{children}</div>
       </div>
     </div>
   );
