c72fd5ff5f5ae007f79fa5b3fa7920d44c1b090c
chosule <89799325+chosule@users.noreply.github.com>
2024-07-05 23:12:40 +0900

    💄 Style: modal css 변경

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -5,14 +5,17 @@ import { useState } from "react";
 type Props = {
   children: React.ReactNode;
   onClickHandler?: () => void;
+  className?: string;
 };
-export function Modal({ children, onClickHandler }: Props) {
+export function Modal({ children, onClickHandler, className }: Props) {
   const [close, setClose] = useState<boolean>(false);
   return (
     <div
       className={`fixed left-0 top-0 z-[4] h-full w-full bg-[rgba(0,0,0,0.4)] ${close && "hidden"}`}
     >
-      <div className="absolute left-[50%] top-[50%] h-[500px] w-[400px] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-white p-[11px]">
+      <div
+        className={`absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] rounded-xl bg-white ${className} p-6`}
+      >
         <div className="flex justify-end">
           <button
             type="button"
@@ -26,7 +29,7 @@ export function Modal({ children, onClickHandler }: Props) {
             <IoClose className="text-[30px]" />
           </button>
         </div>
-        <div className="h-full px-[30px] py-[30px]">{children}</div>
+        <div className="h-full">{children}</div>
       </div>
     </div>
   );
