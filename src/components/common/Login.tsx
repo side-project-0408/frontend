55b431bbc33769144f253e9a331ef7faf887a9f0
chosule <89799325+chosule@users.noreply.github.com>
2024-07-05 23:09:41 +0900

    💄 Style: 로그인 css 수정

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -1,16 +1,17 @@
 "use client";
+import Image from "next/image";
 
 export default function Login() {
   return (
-    <div className="flex h-full flex-1 flex-col justify-center">
-      <section>
-        <div className="text-xl font-extrabold">
-          나에게 딱 맞는 팀 프로젝트 찾을 땐,
+    <div className="flex h-full flex-1 flex-col justify-around">
+      <section className="flex flex-col items-center gap-4">
+        <Image src="/logo.svg" alt="매치메이트 로고" width={100} height={100} />
+        <div className="whitespace-pre-line text-center text-xl font-extrabold">
+          {`나에게 딱 맞는 팀 프로젝트 찾을 땐, \n 메치메이트`}
         </div>
-        <div className="text-xl font-extrabold">매치메이트</div>
       </section>
       <button
-        className="h-[50px] w-full border border-black"
+        className="h-[50px] w-full rounded-md bg-[#fae000] font-semibold"
         onClick={async () => {
           window.location.href =
             "https://api.match-mate.store/oauth2/authorization/kakao";
