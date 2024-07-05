03643303cf7f0670611ba2bf2890c88720cd5a12
chosule <89799325+chosule@users.noreply.github.com>
2024-07-05 23:13:14 +0900

    🐛 Fix: hotpeople searchParams 제거

--- src/components/people/HotPeople.tsx
+++ src/components/people/HotPeople.tsx
@@ -3,24 +3,18 @@ import { QueryClient, useQuery } from "@tanstack/react-query";
 import PeopleContentBox from "./PeopleContentBox";
 import hotPeopls from "@/lib/people/hotPeoples";
 
-type Props = {
-  searchParams: {
-    size: string;
-  };
-};
-
-export default async function HotPeople({ searchParams }: Props) {
+export default async function HotPeople() {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
-    queryKey: ["get", "hotpeoples", searchParams],
+    queryKey: ["get", "hotpeoples"],
     queryFn: hotPeopls,
   });
 
   const hotPeopleData: GetPeoples | undefined = queryClient.getQueryData([
     "get",
     "hotpeoples",
-    searchParams,
   ]);
+
   return (
     <>
       {hotPeopleData?.data?.map((contentData) => (
