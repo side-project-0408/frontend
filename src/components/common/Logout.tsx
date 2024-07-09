321d4e1abd87bc52eb577057ad0e62086e51b59a
chosule <89799325+chosule@users.noreply.github.com>
2024-07-09 14:38:43 +0900

    ✨ Add: 로그아웃 api 요청 및 모달 적용

---
+++ src/components/common/Logout.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useMutation } from "@tanstack/react-query";
+import { deleteCookie, getCookie } from "cookies-next";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Logout() {
+  const router = useRouter();
+
+  const logout = useMutation({
+    mutationFn: async (refreshToken: string) => {
+      await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/token`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+        },
+      });
+    },
+    onSuccess: () => {
+      const accessToken = getCookie("access_token");
+      const refreshToken = getCookie("refresh_token");
+      if (accessToken) {
+        deleteCookie("access_token");
+      }
+      if (refreshToken) {
+        deleteCookie("refresh_token");
+      }
+      router.push("/");
+    },
+  });
+
+  useEffect(() => {
+    const refreshToken = getCookie("refresh_token");
+    if (refreshToken) {
+      logout.mutate(refreshToken);
+    } else {
+      router.push("/");
+    }
+  }, [router]);
+
+  return (
+    <div className="flex flex-col items-center gap-[45px]">
+      <Image src="/logo.png" alt="로고" width={100} height={100} />
+      <p className="text-[17px] font-semibold">
+        로그아웃 되어 메인 페이지로 이동합니다.
+      </p>
+    </div>
+  );
+}
