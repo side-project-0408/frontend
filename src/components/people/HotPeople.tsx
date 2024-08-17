90b786b7d808b85ec556833bcc2e71988d624b9f
chosule <89799325+chosule@users.noreply.github.com>
2024-08-17 11:57:50 +0900

    🔥 Delete : 주석 제거

--- src/components/people/HotPeople.tsx
+++ src/components/people/HotPeople.tsx
@@ -1,21 +1,10 @@
 "use client";
 import { GetPeoples } from "@/model/peoples";
-import { QueryClient, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import PeopleContentBox from "./PeopleContentBox";
 import hotPeopls from "@/lib/people/hotPeoples";
 
 export default function HotPeople() {
-  // const queryClient = new QueryClient();
-  // await queryClient.prefetchQuery({
-  //   queryKey: ["get", "hotpeoples"],
-  //   queryFn: hotPeopls,
-  // });
-
-  // const hotPeopleData: GetPeoples | undefined = queryClient.getQueryData([
-  //   "get",
-  //   "hotpeoples",
-  // ]);
-
   const { data: hotPeopleData } = useQuery<GetPeoples, Error>({
     queryKey: ["get", "hotpeoples"],
     queryFn: hotPeopls,
