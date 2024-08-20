9055e8e456dd8e3e9ed50aab66835b712111d188
chosule <89799325+chosule@users.noreply.github.com>
2024-08-20 09:18:41 +0900

    🐛 Fix : peopleContentBox style 수정

--- src/components/people/PeopleContentBox.tsx
+++ src/components/people/PeopleContentBox.tsx
@@ -23,13 +23,12 @@ export default function PeopleContentBox({ contentData }: Props) {
     softSkill,
     favoriteCount,
   } = contentData;
-  console.log("contentData", contentData);
 
   return (
     <div className="flex gap-4">
       <Link
         href={`/people/${userId}`}
-        className="relative flex h-[308px] w-[288px] flex-col rounded-2xl border-2 bg-neutral-white-0 p-5 shadow-lg transition-transform hover:-translate-y-1 hover:border-orange-500"
+        className={`relative flex h-[340px] w-[288px] flex-col rounded-2xl border-[1px] bg-neutral-white-0 p-5 shadow-lg transition-transform hover:-translate-y-1 ${recent ? "border-neutral-orange-500" : "border-[#c4c4c4]"}`}
       >
         {recent && (
           <Image
@@ -39,10 +38,10 @@ export default function PeopleContentBox({ contentData }: Props) {
           />
         )}
         <div
-          className={`${recent ? "mt-[50px] gap-2" : "mt-0"} flex h-full flex-col justify-between `}
+          className={`${recent ? "mt-[50px] gap-2" : "mt-0"} flex h-full flex-col justify-between gap-5`}
         >
           <div className="flex justify-between">
-            <div className="relative h-10 w-10">
+            <div className="relative h-14 w-14">
               <Image
                 src={userFileUrl}
                 alt="유저프로필"
@@ -65,15 +64,15 @@ export default function PeopleContentBox({ contentData }: Props) {
             </ul>
           </div>
           <ul className="leading-[2]">
-            <li className="whitespace-pre-line text-sm">
+            <li className="text-md whitespace-pre-line">
               <h2>안녕하세요 👋🏻 </h2>
             </li>
             <li>
               <span className="text-sm">저는 </span>
-              <span className="font-bold">{position} 개발자</span>
+              <span className="text-xl font-bold">{position} 개발자</span>
             </li>
             <li className="text-sm">
-              <span className="font-bold">{nickname}</span>
+              <span className="text-xl font-bold">{nickname}</span>
               <span> 입니다.</span>
             </li>
           </ul>
